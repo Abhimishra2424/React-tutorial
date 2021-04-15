d468eaf124155de1b190f7f875d9f087062d0bb0
Abhishek Mishra <abmishra700@gmail.com>
2021-04-15 13:57:03 +0530

    Create a Simple Web App on Top 3 Netflix Series List using JSX

--- src/App.js
+++ src/App.js
@@ -3,23 +3,22 @@ import React from "react";
 function App() {
   return (
     <>
-      <h1>React fragment</h1>
+      <h1>Abhishek Mishra Netflix Pick</h1>
       <p>
-        Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry's standard dummy text ever
-        since the 1500s, when an unknown printer took a galley of type and
-        scrambled it to make a type specimen book. It has survived not only five
-        centuries, but also the leap into electronic typesetting, remaining
-        essentially unchanged. It was popularised in the 1960s with the release
-        of Letraset sheets containing Lorem Ipsum passages, and more recently
-        with desktop publishing software like Aldus PageMaker including versions
-        of Lorem Ipsum.
+        You can watch Netflix through any internet-connected device that offers
+        the Netflix app, including smart TVs, game consoles, streaming media
+        players, set-top boxes, smartphones, and tablets. You can also watch
+        Netflix on your computer using an internet browser. You can review the
+        system requirements for web browser compatibility, and check our
+        internet speed recommendations to achieve the best performance. Need
+        help getting set up? Search our Help Center for the manufacturer of the
+        device you're using.
       </p>
-      <ul>
-        <li>hero</li>
-        <li>hero</li>
-        <li>hero</li>
-      </ul>
+      <ol>
+        <li>Dark</li>
+        <li>Extra Curricular</li>
+        <li>Mr Robot</li>
+      </ol>
     </>
   );
 }
