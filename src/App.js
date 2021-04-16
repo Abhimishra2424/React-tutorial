e1e4fbeec3569d0ce07ea60c7f6d56fd229852c6
Abhishek Mishra <abmishra700@gmail.com>
2021-04-16 13:05:38 +0530

    JavaScript Expressions in JSX in ReactJs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
 import React from "react";
 
+const a = 20;
+const b = 20;
+const sum = a + b;
+
 function App() {
   return (
-    <>
-      <h1>Abhishek Mishra Netflix Pick</h1>
-      <p>
-        You can watch Netflix through any internet-connected device that offers
-        the Netflix app, including smart TVs, game consoles, streaming media
-        players, set-top boxes, smartphones, and tablets. You can also watch
-        Netflix on your computer using an internet browser. You can review the
-        system requirements for web browser compatibility, and check our
-        internet speed recommendations to achieve the best performance. Need
-        help getting set up? Search our Help Center for the manufacturer of the
-        device you're using.
-      </p>
-      <ol>
-        <li>Dark</li>
-        <li>Extra Curricular</li>
-        <li>Mr Robot</li>
-      </ol>
-    </>
+    <h1> 20 + 20  = {sum} </h1>
   );
 }
 
