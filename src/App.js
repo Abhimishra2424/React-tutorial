61481034463cda2e1f2c4b96df5f8846183378a9
Abhishek Mishra <abmishra700@gmail.com>
2021-04-16 13:21:01 +0530

    JavaScript Expressions in JSX in ReactJs of Math abs and round

--- src/App.js
+++ src/App.js
@@ -6,7 +6,11 @@ const sum = a + b;
 
 function App() {
   return (
-    <h1> 20 + 20  = {sum} </h1>
+    <>
+    <h1> 20 + 20  = {sum} </h1>,
+    <h1>{Math.abs(3/5)}</h1>.
+    <h1>{Math.round(5/3)}</h1>.
+    </>
   );
 }
 
