170c8a856cd92e45249619564d2e1d18d761f53e
Abhishek Mishra <abmishra700@gmail.com>
2021-04-24 10:48:22 +0530

    JSX Attributes in ReactJS in Hindi | HTML Attribute Vs JSX Attribute in React and links and add images

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import React from "react";
 
 
-const dayDate = new Date ;
-const monthDate = new Date ;
-const yearDate = new Date ;
-
+const img1 = "https://picsum.photos/seed/picsum/200/300";
+const img2 = "https://picsum.photos/seed/picsum/200/300";
+const img3 = "https://picsum.photos/seed/picsum/200/300";
+const img4 = "https://picsum.photos/seed/picsum/200/300";
+const links = "https://abhi-web.netlify.app/"
 
 function App() {
-  
   return (
-   <>
-     <h1>{`${dayDate.getDate()} ${monthDate.getMonth()} ${yearDate.getFullYear()} `}</h1>
-   </>
+    <>
+      <h1 contentEditable="true">abhishek</h1>
+       <img src={img1} alt="imgss" />
+       <img src={img2} alt="imgss" />
+       <img src={img3} alt="imgss" />
+       <img src={img4} alt="imgss" /> <br />
+       <a href={links}  target="_abhi">My website</a>
+    </>
   );
 }
 
