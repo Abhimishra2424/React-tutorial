4d9e6978a37a4a10ec208694673941809df089f7
Abhishek Mishra <abmishra700@gmail.com>
2021-04-27 17:59:40 +0530

    Internal CSS & Inline CSS Styling In React JS

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import React from "react";
-import './App.css'
+import "./App.css";
 
-const name = 'Abhishek Mishra'
-const img = 'https://picsum.photos/id/1/200/300'
-const img2 = 'https://picsum.photos/id/1/200/300'
+const name = "Abhishek Mishra";
+const img = "https://picsum.photos/id/1/200/300";
+const img2 = "https://picsum.photos/id/1/200/300";
+
+const heading = {
+  color: "blue",
+  marginTop: "70px",
+  textShadow: "0px 2px 4px #00c3ff",
+};
 
 function App() {
   return (
     <>
-      <h1 className="names"> my name is {name} </h1>
+      <h1 style={heading} className="names">
+        My Name Is {name}
+      </h1>
       <div className="main">
-        <img className="img" src={img}/>
-        <img className="img" src={img2}/>
+        <img className="img" src={img} />
+        <img className="img" src={img2} />
       </div>
     </>
   );
