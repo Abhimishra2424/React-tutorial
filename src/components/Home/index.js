b07541e8739a577c01f975579c9832d362ce8afc
Abhishek Mishra <abmishra700@gmail.com>
2021-05-27 14:20:48 +0530

    useState and useEffact and useRef hooks

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,8 +1,24 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useEffect, useRef, useState } from 'react'
+// import { Link } from 'react-router-dom'
 
 export default function Home() {
+  const h1Ref = useRef()
+  
+
+  const [counter , setCounter] = useState(0)
+  const [counter2 , setCounter2] = useState(0)
+
+  useEffect(()=>{
+      console.log(h1Ref);
+  },[counter , counter2])
+   
     return (
-       <h1>Home , go to  <Link  to="/about" >about</Link></h1>
+      <div ref={h1Ref}>
+       <h1>{counter}</h1>
+       <h1>{counter2}</h1>
+
+       <button onClick={()=> setCounter(counter => counter + 1)}> counter1</button>
+       <button onClick={()=> setCounter2(counter => counter + 1)}>conuter2</button>
+      </div>
     )
 }
